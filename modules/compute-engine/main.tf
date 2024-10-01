14ebb14dcc73bbfd53ee7e3dd74f21c5a00bf284
yurehn <yurehn.daniel@gmail.com>
2024-10-01 07:28:23 -0300

    Se agrega creacion de maquina virtual para backend

--- modules/compute-engine/main.tf
+++ modules/compute-engine/main.tf
@@ -2,6 +2,12 @@ locals {
   default_vm_service_account = var.vm_service_account
   vm_machines                = jsondecode(file("${path.module}/vm-machines.json"))
   vm_zone                    = "${var.region_name}-a"
+
+  # Mapa para asignar scripts de inicio según el nombre de la máquina
+  startup_scripts = {
+    "virtual-machine-pokeapi"        = "${path.module}/startupScripts/startup-script-front.sh"
+    "virtual-machine-pokeapi-backend" = "${path.module}/startupScripts/startup-script-back.sh"
+  }
 }
 
 resource "google_compute_instance" "default" {
@@ -22,19 +28,18 @@ resource "google_compute_instance" "default" {
       image = local.vm_machines[count.index].image
     }
   }
-  
+
   labels = {
     my_label = "virtual-machine-pokeapi"
   }
 
   network_interface {
     network = "default"
-    access_config {
-      // Configuration for access config
-    }
+    access_config {}
   }
 
-  metadata_startup_script = file("${path.module}/startup-script")
+  # Ejecutar el script de inicio correspondiente
+  metadata_startup_script = file(local.startup_scripts[local.vm_machines[count.index].name])
 
   service_account {
     email  = local.default_vm_service_account
