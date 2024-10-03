b9a9735afa8691a363f622d2c7f3695cf475160d
yurehn <yurehn.daniel@gmail.com>
2024-10-03 19:58:11 -0300

    se deja maquina virtual de front

--- modules/compute-engine/main.tf
+++ modules/compute-engine/main.tf
@@ -2,12 +2,6 @@ locals {
   default_vm_service_account = var.vm_service_account
   vm_machines                = jsondecode(file("${path.module}/vm-machines.json"))
   vm_zone                    = "${var.region_name}-a"
-
-  # Mapa para asignar scripts de inicio según el nombre de la máquina
-  startup_scripts = {
-    "virtual-machine-pokeapi"        = "${path.module}/startupScripts/startup-script-front.sh"
-    "virtual-machine-pokeapi-backend" = "${path.module}/startupScripts/startup-script-back.sh"
-  }
 }
 
 resource "google_compute_instance" "default" {
@@ -28,18 +22,19 @@ resource "google_compute_instance" "default" {
       image = local.vm_machines[count.index].image
     }
   }
-
+  
   labels = {
     my_label = "virtual-machine-pokeapi"
   }
 
   network_interface {
     network = "default"
-    access_config {}
+    access_config {
+      // Configuration for access config
+    }
   }
 
-  # Ejecutar el script de inicio correspondiente
-  metadata_startup_script = file(local.startup_scripts[local.vm_machines[count.index].name])
+  metadata_startup_script = file("${path.module}/startup-script-front")
 
   service_account {
     email  = local.default_vm_service_account
